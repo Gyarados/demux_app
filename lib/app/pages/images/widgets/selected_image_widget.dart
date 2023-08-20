99a6b8af5f5d45d3c319c997b90abaaf7a5e4909
Gabriel Desiderá <gabriel.desidera@hotmail.com>
2023-08-20 20:58:45 -0300

    Selected image bold

--- lib/app/pages/images/widgets/selected_image_widget.dart
+++ lib/app/pages/images/widgets/selected_image_widget.dart
@@ -21,18 +21,24 @@ class SelectedImageWidget extends StatelessWidget {
         Container(
             decoration: BoxDecoration(
                 color: Colors.grey.shade300,
-                borderRadius:
-                    BorderRadius.vertical(top: Radius.circular(10))),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
             child: Row(
               children: [
-                SizedBox(width: 8,),
-                Expanded(child:Text("Selected image", textAlign: TextAlign.start,)),
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                    child: Text(
+                  "Selected image",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                )),
                 IconButton(
                   padding: EdgeInsets.all(0),
                   onPressed: loadingResults ? null : closeButtonOnPressed,
                   icon: Icon(
-                Icons.close,
-                color: Colors.red,
+                    Icons.close,
+                    color: Colors.red,
                   ),
                 ),
               ],
