41089171640025ffd708b933f36e7d95b5b1bfd4
Gabriel Desiderá <gabriel.desidera@hotmail.com>
2023-10-16 15:29:04 -0300

    Add web support

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
-      storageDirectory: await getTemporaryDirectory());
+      storageDirectory: kIsWeb
+      ? HydratedStorage.webStorageDirectory
+      : await getTemporaryDirectory(),);
 
   runApp(MultiBlocProvider(
     providers: [
